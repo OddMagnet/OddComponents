2bec9dfb54c4e22b31574ca3c179585d1fa926d2
OddMagnet <mibruenen@gmail.com>
2020-11-17 05:22:35 +0100

    Initial commit

---
+++ OddComponents/OddComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddComponentsApp.swift
+//  OddComponents
+//
+//  Created by Michael Brünen on 17.11.20.
+//
+
+import SwiftUI
+
+@main
+struct OddComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
