e9e541ee3d668307f75e56f5838b1a4ab8dd7152
OddMagnet <mibruenen@gmail.com>
2020-12-06 12:34:15 +0100

    Remove Animation and Color from Codable extensions

--- ParticleView/Codable-Extensions.swift
+++ ParticleView/Codable-Extensions.swift
@@ -18,34 +18,61 @@ extension Angle: Codable {
     }
 }
 
-extension Animation: Codable {
-    // TODO: - Codable extension
-    public init(from decoder: Decoder) throws {
-        
-    }
-    public func encode(to encoder: Encoder) throws {
-
-    }
-}
-
 extension BlendMode: Codable {
     // TODO: - Codable extension
     public init(from decoder: Decoder) throws {
-        
-    }
-    public func encode(to encoder: Encoder) throws {
-
-    }
-}
-
-extension Color: Codable {
-    public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        self.init(try container.decode(String.self))
+        switch try container.decode(String.self) {
+            case "color": self = .color
+            case "colorBurn": self = .colorBurn
+            case "colorDodge": self = .colorDodge
+            case "darken": self = .darken
+            case "destinationOut": self = .destinationOut
+            case "destinationOver": self = .destinationOver
+            case "difference": self = .difference
+            case "exclusion": self = .exclusion
+            case "hardLight": self = .hardLight
+            case "hue": self = .hue
+            case "lighten": self = .lighten
+            case "luminosity": self = .luminosity
+            case "multiply": self = .multiply
+            case "overlay": self = .overlay
+            case "plusDarker": self = .plusDarker
+            case "plusLighter": self = .plusLighter
+            case "saturation": self = .saturation
+            case "screen": self = .screen
+            case "softLight": self = .softLight
+            case "sourceAtop": self = .sourceAtop
+            default: self = .normal
+        }
     }
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
-        try container.encode(self.description)
+        var value = ""
+        switch self {
+            case .color: value = "color"
+            case .colorBurn: value = "colorBurn"
+            case .colorDodge: value = "colorDodge"
+            case .darken: value = "darken"
+            case .destinationOut: value = "destinationOut"
+            case .destinationOver: value = "destinationOver"
+            case .difference: value = "difference"
+            case .exclusion: value = "exclusion"
+            case .hardLight: value = "hardLight"
+            case .hue: value = "hue"
+            case .lighten: value = "lighten"
+            case .luminosity: value = "luminosity"
+            case .multiply: value = "multiply"
+            case .overlay: value = "overlay"
+            case .plusDarker: value = "plusDarker"
+            case .plusLighter: value = "plusLighter"
+            case .saturation: value = "saturation"
+            case .screen: value = "screen"
+            case .softLight: value = "softLight"
+            case .sourceAtop: value = "sourceAtop"
+            default: value = "normal"
+        }
+        try container.encode(value)
     }
 }
 
